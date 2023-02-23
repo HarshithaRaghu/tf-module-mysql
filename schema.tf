37f30a956b11195ae9054e7f08a96dec2c95addc
Harshitha <harshithadarshan95@gmail.com>
2023-02-23 10:51:20 +0530

    Autopush

--- schema.tf
+++ schema.tf
@@ -1,11 +1,14 @@
-resource "null_resource" "mysql5-schema" {
+resource "null_resource" "mysql-schema" {
   
-##This is how we can create depenency and ensure this will only run after the creation if the RDS Instance.
-depends_on = [aws_db_instance.mysql5]
+  # This is how we can create depenency and ensure this will only run after the creation if the RDS Instance.
+  depends_on = [aws_db_instance.mysql5]
 
   provisioner "local-exec" {
         command = <<EOF
+        cd /tmp 
         curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+        unzip -o mysql.zip 
+        cd mysql-main 
         mysql -h ${aws_db_instance.mysql5.address} -u admin1 -pRoboShop1 <shipping.sql
         EOF
   }  
