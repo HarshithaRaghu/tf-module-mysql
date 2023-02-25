20b072b8a630cd7456418f54a304db452bdb9756
Harshitha <harshithadarshan95@gmail.com>
2023-02-25 12:43:43 +0530

    Autopush

--- data.tf
+++ data.tf
@@ -7,3 +7,12 @@ data "terraform_remote_state" "vpc" {
         region = "us-east-1"
   }
 }
+
+# fetching the metadata of the secret
+data "aws_secretsmanager_secret" "secrets" {
+  name = "roboshop/secrets"
+}
+
+data "aws_secretsmanager_secret_version" "secrets" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
+}
